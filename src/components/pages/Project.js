323478f904d79371b207f4a6f2b4efa79d63c0a8
andreenakashima <andreenakashima@gmail.com>
2022-07-14 15:12:37 -0300

    Exibindo detalhes do projeto

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,14 +1,19 @@
-// import styles from './Project.module.css'
+import styles from './Project.module.css'
 
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
+import Loading from '../layout/Loading'
+import Container from '../layout/Container'
+
 function Project() {
     const { id } = useParams()
     const [ project, setProject ] = useState([])
+    const [ showProjectForm, setShowProjectForm ] = useState( false )
 
     useEffect(() => {
-        fetch(`http://localhost:5000/projects/${id}`, {
+        setTimeout(() => {
+            fetch(`http://localhost:5000/projects/${id}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
@@ -18,12 +23,50 @@ function Project() {
             setProject(data)
         })
         .catch((err) => console.log(err))
+        }, 300)
     }, [id])
 
+    function toggleProjectForm() {
+        setShowProjectForm(!showProjectForm)
+    }
+
     return (
-        <div>
-            { project.name }
-        </div>
+        <>
+        {project.name ? (
+            <div className={ styles.project_details }>
+                <Container customClass="column">
+                    <div className={ styles.details_container }>
+
+                        <h1>Projeto: { project.name }</h1>
+
+                        <button className={ styles.btn } onClick={ toggleProjectForm }>
+                            { !showProjectForm ? 'Editar projeto' : 'Fechar' }
+                        </button>
+
+                        {!showProjectForm ? (
+                            <div className={ styles.project_info }>
+                                <p>
+                                    <span>Categoria:</span> { project.category.name }
+                                </p>
+                                <p>
+                                    <span>Total de orçamento:</span> R${ project.budget }
+                                </p>
+                                <p>
+                                    <span>Total ultilizado:</span> R${ project.cost }
+                                </p>
+                            </div>
+                        ) : (
+                            <div className={ styles.project_info }>
+                                <p>Form</p>
+                            </div>
+                        )}
+                    </div>
+                </Container>
+            </div>
+        ) : (
+            <Loading />
+        )}
+        </>
     )
 }
 
